072564173e9623f5b883d8bba490b8769f4f2e58
Julian <julianwendland@gmx.de>
2021-04-19 17:43:06 +0200

    comments

--- dispatscherWorkflow/Final-ExecuteWorkflowsUsingHoKAuth.ps1
+++ dispatscherWorkflow/Final-ExecuteWorkflowsUsingHoKAuth.ps1
@@ -1,8 +1,6 @@
 ﻿
-# In order to test this, you need to get vsphere SDK for webservices, as this is using the libraries from there. https://my.vmware.com/group/vmware/downloads/get-download?downloadGroup=VS-MGMT-SDK70U2
+# In order to test this, you need to get vsphere SDK for webservices, as this is using the VMware.Binding.WsTrust.dll, STSService.dll libraries from there.
 # new-webserviceproxy can't handle soap security headers. You also need PKCS#12 pfx certificate (in this example)
-
-
 
 $vroURL = "https://vro8.vdi.sclabs.net:443"
 $vcURL= "https://jw-vcsa7.vdi.sclabs.net:7444"
@@ -16,16 +14,16 @@ $ApiEndpoint= "/vco/api/workflows/$($wfid)/executions"
 $restmethod = 'POST'
 $apiFormat = 'xml' 
 
-
+# Add types from vSphere SDK https://my.vmware.com/group/vmware/downloads/get-download?downloadGroup=VS-MGMT-SDK70U2
 Add-Type -Path 'C:\Users\JW\Desktop\SDK\vsphere-ws\dotnet\bin\VMware-SDK-Management-SampleTemplate\bin\Debug\VMware.Binding.WsTrust.dll'
 Add-Type -Path 'C:\Users\JW\Desktop\SDK\vsphere-ws\dotnet\bin\VMware-SDK-Management-SampleTemplate\bin\Debug\STSService.dll'
 
 
-
+ # settings 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [VMware.Binding.WsTrust.SamlTokenHelper]::SetupServerCertificateValidation()
 
-#https://www.dorkbrain.com/docs/2017/09/02/gzip-in-powershell/
+# Fuction ConvertTo-GZipString Source: https://www.dorkbrain.com/docs/2017/09/02/gzip-in-powershell/
 Function ConvertTo-GZipString () {
     [CmdletBinding()]
     Param(
@@ -44,65 +42,61 @@ Function ConvertTo-GZipString () {
 }
 
 
-#i have generated my certificate with 'VMware1!' password
+# Import "MachineKeySet" from certificate 
 $signingCertificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
 $signingCertificate.Import($certPath, $certPass, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::MachineKeySet)
 
-$service = [VMware.Binding.WsTrust.SamlTokenHelper]::GetSTSService("$vcURL/sts/STSService/lab.local", $username, $password, $signingCertificate) 
-write-host -ForegroundColor Green "Auth URL: $vcURL/sts/STSService/lab.local"
+# Create token 
+$service = [VMware.Binding.WsTrust.SamlTokenHelper]::GetSTSService("$vcURL/sts/STSService", $username, $password, $signingCertificate) 
 $token = [VMware.Binding.WsTrust.SamlTokenHelper]::GetHokRequestSecurityTokenType()
 $token.SignatureAlgorithm = [vmware.sso.SignatureAlgorithmEnum]::httpwwww3org200104xmldsigmorersasha256
 $response = $service.Issue($token)
-
 $responsetoken = $response.RequestSecurityTokenResponse.RequestedSecurityToken
 $responsetokenXML = $responsetoken.OuterXml
+
+# Convert to GZip string
 $encodedANDgzippedtoken = ConvertTo-GZipString –String $responsetokenXML
 
-#I had new line like that before but `n works as well.
-#$nl = (0x0A -as [char]) 
-
+# Build normalized request string. Please find the details below shared by the engineering team.
 $timestamp = [DateTimeOffset]::Now.ToUnixTimeSeconds().ToString()
 $nonce = $timestamp + ':ass234'
-
 [system.uri]$uri = "$vroURL$ApiEndpoint"
-$httprequesturi = '/' + $uri.AbsolutePath.split('/')[-1] + $uri.Query
 $httprequesthost = $uri.Host
 $httprequestport = $uri.Port
 $noext = ''
 
-$normalizedrequeststring = $timestamp + "`n" + $nonce + "`n" + $timestamp + "`n" + $restmethod + "`n" + $httprequesturi + "`n" + $httprequesthost + "`n" + $httprequestport + "`n" + $noext + "`n"
+$normalizedrequeststring = $timestamp + "`n" + $nonce + "`n" + $timestamp + "`n" + $restmethod + "`n" + $ApiEndpoint + "`n" + $httprequesthost + "`n" + $httprequestport + "`n" + $noext + "`n"
 
-
+# Import "Exportable" from certificate 
 $psigningCertificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
 $psigningCertificate.Import($certPath, $certPass, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable  )
-#converted from c# to PS from https://stackoverflow.com/questions/7444586/how-can-i-sign-a-file-using-rsa-and-sha256-with-net
+
+# Converted from c# to PS from https://stackoverflow.com/questions/7444586/how-can-i-sign-a-file-using-rsa-and-sha256-with-net
 $privatekey = $psigningCertificate.PrivateKey
 $privatekey1 = New-Object System.Security.Cryptography.RSACryptoServiceProvider  
 $privatekey1.ImportParameters($privatekey.ExportParameters($true))
 
+# base64signature 
 $enc = [system.Text.Encoding]::UTF8
 $data = $enc.GetBytes($normalizedrequeststring) 
 $sig = $privatekey1.SignData($data, "SHA256")
 $base64sig = [Convert]::ToBase64String($sig)
 #[bool]$isValid = $privateKey1.VerifyData($data, "SHA256", $sig)
 
-
+# request header
 $headervalue = 'SIGN token="{0}",nonce="{1}",signature_alg="RSA-SHA256",signature="{2}"' -f $encodedANDgzippedtoken, $nonce, $base64sig
 $header = @{'Authorization' = $headervalue;"Content-Type"="application/$($apiFormat)";"Accept"="application/$($apiFormat)" }
 
+# Workflow input parameter from xml 
 $jsonBody = Get-Content $inputFile -Raw
 
+# Start API-ServerDeployment workflow with input parameters.
 $answer = Invoke-WebRequest –Uri "$vroURL$ApiEndpoint" –Headers $header -Body $jsonBody -Method $restmethod
 $answer.Content 
 
 
 
-
-
-
 <#
-
-Please find the details below shared by the engineering team.
 The Authorization header has the following.
 Authorization: SIGN token="…",
                nonce="1589541389518:1761545587",
@@ -126,6 +120,5 @@ signature          REQUIRED. A message signature calculated over the normalized 
                              BASE64 applied and (b) no "ext" field is appended. All text based fields in the normalized request
                              are encoded in UTF-8.
 
-Source: https://grzegorzkulikowski.info/2020/05/20/vrealize-orchestrator-rest-request-using-hok-token/
-
+# Source: https://grzegorzkulikowski.info/2020/05/20/vrealize-orchestrator-rest-request-using-hok-token/
 #>
